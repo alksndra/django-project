e7b3ae2c73ca97115d03a3420fc138cb3a82b4ff
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-11-25 09:08:39 +0200

    fix names_list

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import  models
+from django.template import loader
 
 
 def index(request):
@@ -14,7 +15,21 @@ def index_name(request, name):
 
 
 def names_list(request):
-    return HttpResponse(models.Name.objects.all())
+    all_names_list = models.Name.objects.all()
+    template = loader.get_template('template.html')
+    context = {
+        'all_names_list': all_names_list,
+    }
+    return render(request, 'template.html', context)
+
+"""
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    
+"""
 
 
 
